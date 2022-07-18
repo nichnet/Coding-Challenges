2c87df455340eb32c022fa0049ae586e51bb06b6
Nick <19955751+nichnet@users.noreply.github.com>
2022-07-18 12:46:31 +0930

    Create To-Lowercase.py

---
+++ python/To-Lowercase.py
@@ -0,0 +1,73 @@
+#Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+class Solution(object):
+    def toLowerCase(self, s):
+        lower = [
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z'
+        ]
+
+        upper = [
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z'
+        ]
+
+        
+        out = ""
+        
+        for i in s:
+            if i in upper:
+                out += lower[upper.index(i)]
+            else:
+                out += i
+        
+        return out
+        
