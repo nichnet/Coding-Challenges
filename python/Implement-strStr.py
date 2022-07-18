03612c22a5cc388a9de87b37bc6cf6359ec33f8e
Nick <19955751+nichnet@users.noreply.github.com>
2022-07-18 12:52:30 +0930

    Update Implement-strStr.py

--- python/Implement-strStr.py
+++ python/Implement-strStr.py
@@ -1,3 +1,5 @@
+#Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack
+
 class Solution(object):
     def strStr(self, haystack, needle):
         needle_len = len(needle) #length of the needle
