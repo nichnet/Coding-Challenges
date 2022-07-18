ea56fae9deb656e177f00322329bfedb348f0675
Nick <19955751+nichnet@users.noreply.github.com>
2022-07-18 12:40:35 +0930

    Create Implement-strStr.py

---
+++ python/Implement-strStr.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        needle_len = len(needle) #length of the needle
+
+        for i in range (len(haystack)): #iterate each character in haystack
+            
+            xx = 0
+            matched = 0 #number of matched chars
+            
+            subrange = i + needle_len #length of i index + length of needle 
+
+            if subrange > len(haystack):
+                break #break if out of bounds
+
+            for j in range(i, subrange, 1): #iterate sub string
+                if haystack[j] == needle[xx]: #if matched , add
+                    matched += 1
+
+                xx += 1 #iterate to next needle char
+        
+            if matched == len(needle):
+                return i #return the index if the matched chars is correct
+    
+        return -1
